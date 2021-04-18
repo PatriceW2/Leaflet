92d7c6396b88be3ef9cbf59721c6f94710d21d0c
PatriceW <tolmie2@gmail.com>
2021-04-18 15:09:49 -0700

    changing tile layer and pulling data

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,23 +17,29 @@
 /
 / ----------------------------------------*/
 
-// Creating map object
+//create the map
 const myMap = L.map("map", {
-    center: [34.000, -117.000],
-    zoom: 8
+    center: [37.09, -95.71],
+    zoom: 5,
   });
 
 
+  //add a tile layer
+  //const darkmap = 
+  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  }).addTo(myMap);
 
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
 
-const geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
+
+const geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+
+
+d3.json(geoData).then(data => {
+    console.log(data);
+
+})
 
